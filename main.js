3aef0c0886786ceb25f84fedade26eb208b3ea3f
indianameregone <68615315+indianameregone@users.noreply.github.com>
2021-07-29 22:41:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function portfolio(){
+    document.getElementById('personas').style.display="none"
+    document.getElementById('portfolio').style.display="block"
+    document.getElementById('iframe-1').style.display="flex"
+    document.getElementById('iframe-2').style.display="flex"
+}
+function start(){
+    document.getElementById('personas').style.display="flex"
+    document.getElementById('portfolio').style.display="none"
+    document.getElementById('iframe-1').style.display="none"
+    document.getElementById('iframe-2').style.display="none"
+}
